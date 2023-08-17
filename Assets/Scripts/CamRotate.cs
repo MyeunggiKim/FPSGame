870c2a5909d51eb9a3b7aaac980819866728b9a6
MyeunggiKim <139723168+MyeunggiKim@users.noreply.github.com>
2023-08-17 16:21:21 +0900

    feat: 카메라 회전, 플레이어 회전, 플레이어 이동
    카메라 회전
    -CamRotate.cs 기능추가
    플레이어 회전
    -PlayerRotate.cs 기능추가
    플레이어 이동
    -PlayerMove.cs 기능추가

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//목표: 마우스 입력을 받아 카메라를 회전시킨다.
+//필요속성: 마우스 입력 X, Y, 속도
+//순서1: 마우스 입력을 받는다.
+//순서2: 마우스 입력에 따라 방향을 설정한다.
+//순서3: 물체를 회전시킨다.
+
+
+
+public class CamRotate : MonoBehaviour
+{
+    public float speed = 10.0f;
+    float mx = 0;
+    float my = 0;
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //마우스의 입력.
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        mx += mouseX * speed * Time.deltaTime;
+        my += mouseY * speed * Time.deltaTime;
+
+        my = Mathf.Clamp(my, -90.0f, 90.0f);
+
+        //입력에 따른 회전방향 설정.
+        Vector3 dir =  new Vector3(-my, mx, 0);
+
+        //물체를 회전시킨다.
+        //r = r0 +vt
+        transform.eulerAngles = dir;
+
+    }
+}
